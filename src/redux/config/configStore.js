a899e6ce7639e3831117fb414fa67892c853e401
rlawodnr1109 <wodnr1109@naver.com>
2022-10-20 09:29:37 +0900

    Thunk 연습

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -3,9 +3,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import counter from "../modules/counterSlice";
 
 import todolist from "../modules/todolistSlice";
+import todos from "../modules/todosSlice";
 
 const store = configureStore({
-  reducer: { counter: counter, todolist: todolist },
+  reducer: { counter: counter, todolist: todolist, todos: todos },
 });
 console.log(store);
 
